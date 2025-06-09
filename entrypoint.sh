6bf1db45b75e37199098ad804aa5106d377ab8df
okki-0417 <ochan0901@gmail.com>
2025-06-09 15:47:54 +0900

    fix: seedをコメントあうと

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,7 @@ rm -f tmp/pids/server.pid
 if [ "$RAILS_ENV" = "production" ]; then
   # bundle exec rails db:create
   # bundle exec rails db:migrate
-
-  bundle exec rails db:seed
+  # bundle exec rails db:seed
 fi
 
 exec "$@"
