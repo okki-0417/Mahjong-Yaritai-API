b89e06ddff3eae2fe6fe52b301223bc0f7992bd6
okki-0417 <ochan0901@gmail.com>
2025-06-10 12:13:10 +0900

    debug: authorziations#create

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -5,7 +5,10 @@ class AuthorizationsController < ApplicationController
     authorization = Authorization.find_by(token: token_params[:token])
 
     if authorization && !authorization.expired?
+      Rails.logger.info("authorization: #{authorization}")
+
       session[:authorization_id] = authorization.id
+      Rails.logger.info("session[:authorization_id]: #{session[:authorization_id]}")
 
       head :ok
     else
