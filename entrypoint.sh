74712a5651f2af5cadab1c8b8fcbb8ce1c8ae24d
okki-0417 <ochan0901@gmail.com>
2025-06-23 11:18:08 +0900

    chore: entrypoint.sh cerate DB [skip ci]

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 rm -f tmp/pids/server.pid
 
 if [ "$RAILS_ENV" = "production" ]; then
-#   bundle exec rails db:create
-#   bundle exec rails db:migrate
-#   bundle exec rails db:seed
+  bundle exec rails db:create
+  bundle exec rails db:migrate
+  bundle exec rails db:seed
 
   mkdir -p log
   touch log/production.log
