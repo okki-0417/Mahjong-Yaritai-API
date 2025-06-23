199057567cb580ae096cd9ccc9a1137ddaddd72f
okki-0417 <ochan0901@gmail.com>
2025-06-23 11:03:28 +0900

    debug: env.frontend_origin

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -4,6 +4,7 @@ class HealthCheckController < ApplicationController
   rescue_from(StandardError) { head :internal_server_error }
 
   def show
+    Rails.logger.info("********************* ENV FRONTEND_ORIGIN: #{ENV.fetch("FRONTEND_ORIGIN")}*******************")
     head :ok
   end
 end
