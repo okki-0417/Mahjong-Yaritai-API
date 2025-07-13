4de854bcb983dcfbf694f5dd1423e01d43353969
okki-0417 <ochan0901@gmail.com>
2025-07-13 20:59:13 +0900

    debug: [skip ci]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,6 @@ class SessionsController < ApplicationController
   before_action :restrict_to_logged_in_user, only: %i[destroy]
 
   def show
-    Rails.logger.info "current_user: #{current_user&.inspect}"
-    Rails.logger.info "logged_in?: #{logged_in?}"
-    Rails.logger.info "session: #{session.inspect}"
-    Rails.logger.info "session[:user_id]: #{session[:user_id]}"
-    Rails.logger.info "cookies: #{cookies.inspect}"
-    Rails.logger.info "cookies.signed[:user_id]: #{cookies.signed[:user_id]}"
-    Rails.logger.info "cookies.signed[:remember_token]: #{cookies.signed[:remember_token]}"
-
     render json: current_user,
       serializer: SessionSerializer,
       root: :session,
