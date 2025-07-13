e9219544d49cab61fb090f47a6ae4415cadb8c48
okki-0417 <ochan0901@gmail.com>
2025-07-13 23:42:08 +0900

    fix: sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@ Sidekiq.configure_server do |config|
   config.redis = {
     host: ENV.fetch("REDIS_HOST", "localhost"),
     port: ENV.fetch("REDIS_PORT", 6379).to_i,
-    db: 1
+    db: 1,
   }
 end
 
@@ -10,6 +10,6 @@ Sidekiq.configure_client do |config|
   config.redis = {
     host: ENV.fetch("REDIS_HOST", "localhost"),
     port: ENV.fetch("REDIS_PORT", 6379).to_i,
-    db: 1
+    db: 1,
   }
 end
