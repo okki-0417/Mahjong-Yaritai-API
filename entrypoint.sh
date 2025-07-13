3e786379ab67753218060c6a27b46d874d7429b2
okki-0417 <ochan0901@gmail.com>
2025-07-13 13:03:11 +0900

    migrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ rm -f tmp/pids/server.pid
 
 if [ "$RAILS_ENV" = "production" ]; then
   # bundle exec rails db:create
-  # bundle exec rails db:migrate
+  bundle exec rails db:migrate
   # bundle exec rails db:seed
 
   mkdir -p log
