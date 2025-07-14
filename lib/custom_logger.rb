342a73958a60a1ea2e2d9c2b370b65c48bb95266
okki-0417 <ochan0901@gmail.com>
2025-07-14 11:26:48 +0900

    chore: silence log /session GET

--- lib/custom_logger.rb
+++ lib/custom_logger.rb
@@ -1,9 +1,14 @@
 class CustomLogger < Rails::Rack::Logger
   def call(env)
     user_agent = env["HTTP_USER_AGENT"].to_s
+    request_path = env["PATH_INFO"]
+    request_method = env["REQUEST_METHOD"]
 
+    # デプロイツールからのヘルスチェックのログを出さないため
     if user_agent.include?(ENV.fetch("USER_AGENT"))
-      # デプロイツールからのヘルスチェックのログを出さないため
+      Rails.logger.silence { super }
+    # GET /sessionのログを出さないため（フロントエンドからの頻繁なアクセス）
+    elsif request_method == "GET" && request_path == "/session"
       Rails.logger.silence { super }
     else
       super
