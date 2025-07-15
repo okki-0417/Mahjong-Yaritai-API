712c4906942e035ec86960baf9fcf7ff33001d52
okki-0417 <ochan0901@gmail.com>
2025-07-15 12:02:16 +0900

    fix: ログイン済み会員をログアウトさせる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,20 @@ class ApplicationController < ActionController::API
   include Rails.application.routes.url_helpers
   include ActionController::RequestForgeryProtection
 
+  before_action :force_logout_once
+
   def reject_logged_in_user
     render json: error_json([ "Already Logged In" ]), status: :forbidden if logged_in?
   end
 
   def restrict_to_logged_in_user
     render json: { errors: [ message: "Please Login" ] }, status: :unauthorized unless logged_in?
+  end
+
+  def force_logout_once
+    return unless session[:force_logged_out].blank?
+
+    reset_session
+    session[:force_logged_out] = true
   end
 end
