a645bbb98be12497db4fecacc5698e49a3e5df39
okki-0417 <ochan0901@gmail.com>
2025-09-28 10:21:04 +0900

    chore

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,14 @@ set -e
 
 rm -f tmp/pids/server.pid
 
-# if [ "$RAILS_ENV" = "production" ]; then
+if [ "$RAILS_ENV" = "production" ]; then
   # bundle exec rails db:create
-  # bundle exec rails db:migrate
+  bundle exec rails db:migrate
   # bundle exec rails db:seed
 
   # mkdir -p log
   # touch log/production.log
   # chmod 664 log/production.log
-# fi
+fi
 
 exec "$@"
