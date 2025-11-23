89ea7deab22e8214439875ff414c5a167fa3cdeb
okki-0417 <ochan0901@gmail.com>
2025-11-23 13:33:04 +0900

    fix: 本番環境で何切る問題作成が失敗するデバッグ

--- app/graphql/mutations/what_to_discard_problems/create_what_to_discard_problem.rb
+++ app/graphql/mutations/what_to_discard_problems/create_what_to_discard_problem.rb
@@ -12,21 +12,21 @@ module Mutations
       argument :turn, Integer, required: false
       argument :wind, String, required: false
       argument :points, Integer, required: false
-      argument :dora_id, ID, required: false
-      argument :hand1_id, ID, required: false
-      argument :hand2_id, ID, required: false
-      argument :hand3_id, ID, required: false
-      argument :hand4_id, ID, required: false
-      argument :hand5_id, ID, required: false
-      argument :hand6_id, ID, required: false
-      argument :hand7_id, ID, required: false
-      argument :hand8_id, ID, required: false
-      argument :hand9_id, ID, required: false
-      argument :hand10_id, ID, required: false
-      argument :hand11_id, ID, required: false
-      argument :hand12_id, ID, required: false
-      argument :hand13_id, ID, required: false
-      argument :tsumo_id, ID, required: false
+      argument :dora_id, ID, required: true
+      argument :hand1_id, ID, required: true
+      argument :hand2_id, ID, required: true
+      argument :hand3_id, ID, required: true
+      argument :hand4_id, ID, required: true
+      argument :hand5_id, ID, required: true
+      argument :hand6_id, ID, required: true
+      argument :hand7_id, ID, required: true
+      argument :hand8_id, ID, required: true
+      argument :hand9_id, ID, required: true
+      argument :hand10_id, ID, required: true
+      argument :hand11_id, ID, required: true
+      argument :hand12_id, ID, required: true
+      argument :hand13_id, ID, required: true
+      argument :tsumo_id, ID, required: true
 
       def resolve(
         description: nil,
@@ -52,6 +52,30 @@ module Mutations
       )
         require_authentication!
 
+        Rails.logger.warn("Creating WhatToDiscardProblem by user_id=#{current_user.id}")
+        Rails.logger.warn("Params: #{{
+          description:,
+          round:,
+          turn:,
+          wind:,
+          points:,
+          dora_id:,
+          hand1_id:,
+          hand2_id:,
+          hand3_id:,
+          hand4_id:,
+          hand5_id:,
+          hand6_id:,
+          hand7_id:,
+          hand8_id:,
+          hand9_id:,
+          hand10_id:,
+          hand11_id:,
+          hand12_id:,
+          hand13_id:,
+          tsumo_id:,
+        }.inspect}")
+
         problem = current_user.created_what_to_discard_problems.new(
           description:,
           round:,
@@ -74,6 +98,8 @@ module Mutations
           hand13_id:,
           tsumo_id:,
         )
+
+        Rails.logger.warn("WhatToDiscardProblem to be created: #{problem.attributes.inspect}")
 
         if problem.save
           { what_to_discard_problem: problem }
